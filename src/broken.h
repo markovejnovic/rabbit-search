354d6bb98371c715d39b8bd10fb4fc31bc793333
Marko Vejnovic <marko.vejnovic@hotmail.com>
2024-09-04 23:23:54 -0700

    Broken builds

---
+++ src/broken.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <immintrin.h>
+
+inline static char const* sz_find(char const * haystack, size_t h_length, char const* needle, size_t n_length) {
+    __m256i a, b;
+    a = _mm256_cmpeq_epi8(a, b);
+    return 0;
+}
--- src/str.zig
+++ src/str.zig
@@ -1,7 +1,7 @@
-const cString = @cImport({
-    @cInclude("string.h");
+const cStringZilla = @cImport({
+    @cInclude("broken.h");
 });
 
 pub fn strsearch(needle: []const u8, haystack: []const u8) bool {
-    return cString.memmem(haystack.ptr, haystack.len, needle.ptr, needle.len) != cString.NULL;
+    return cStringZilla.sz_find(haystack.ptr, haystack.len, needle.ptr, needle.len) != 0;
 }
